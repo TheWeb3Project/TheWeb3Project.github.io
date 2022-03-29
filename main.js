64bf11723ac2a793fde0dd02a8c9e5cfac0a8c2f
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-03-29 17:48:08 +0900

    a

--- main.js
+++ main.js
@@ -216,14 +216,14 @@ catch(err) {
 
 const carousel = select('#carousel');
     for (const [idy, itemInfo] of Object.entries(ITEMINFOS)) {
-        let div = makeElem('div', null, 'item col-12 col-lg-4 mt-4 mb-4');
+        let div = makeElem('div', null, 'item col-12 mt-4 mb-4');
 
         let htmlStr = getItemCard(itemInfo);
 
         div.innerHTML = htmlStr;
         carousel.append(div);
     }
-    
+
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
