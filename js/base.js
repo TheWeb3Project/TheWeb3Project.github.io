660b11d8fc0fb5491579f1eb801f2cb47a0aa487
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-03-31 02:21:41 +0900

    a

--- js/base.js
+++ js/base.js
@@ -241,6 +241,33 @@ function displayText(el, text) {
 }
 
 
+
+function setCookie(name, value, expDays) {
+  let date = new Date();
+  date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + "; " + expires + "; path=/";
+}
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+
+
+
 function copy(value) {
   const input = document.createElement('textarea');
   input.value = value;
