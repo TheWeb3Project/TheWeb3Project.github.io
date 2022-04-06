088cc5f29b5a07740544f3c49cb0a17d2b9faa48
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-04-06 21:47:03 +0900

    a

--- js/base.js
+++ js/base.js
@@ -78,6 +78,7 @@ ABIS['web3'] = [
   "function manualRebase()",
   "function _isExperi()",
   "function toggleExperi()",
+  "function buysell(uint)",
   "function _curcuitBreakerFlag() view returns (uint256)",
   "function _curcuitBreakerTime() view returns (uint256)",
   "function _lastRebaseBlock() view returns (uint256)",
