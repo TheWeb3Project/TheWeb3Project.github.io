b9a5ea6ca4ff8a7acdeb1b13749e8cc1b7931c56
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-04-26 16:55:43 +0900

    a

--- js/base.js
+++ js/base.js
@@ -161,6 +161,7 @@ ABIS['web3Miner'] = [
   "function _miners(address) view returns (uint256)",
   "function _daimonds(address) view returns (uint256)",
   "function _tDaimonds() view returns (uint256)",
+  "function calculateWusdRewards(address) view returns (uint256)",
 ];
 
 ADRS['factory'] = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
