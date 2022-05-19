cd1df13a08f57648f0b13a610ba9c96d7b35c85e
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-05-19 12:45:46 +0900

    a

--- js/page.js
+++ js/page.js
@@ -38,6 +38,7 @@ function displaySidebar() {
             ${htmlSide('wrap.html', 'box-seam', 'Wrap')}
             ${htmlSide('web-pointshop.html', 'shop', 'Point Shop')}
             ${htmlSide('miner.html', 'minecart-loaded', 'Miner')}
+            ${htmlSide('wusd.html', 'minecart-loaded', 'WUSD')}
             ${htmlSide('xweb.html', 'minecart-loaded', 'xWEB3')}
             ${htmlSide('faq.html', 'minecart-loaded', 'FAQ')}
             ${htmlSide('https://docs.theweb3project.com', 'journal-text', 'Docs')}
