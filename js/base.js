4230b1ac69bc95cc4cc33ff34d28a55d2820d15c
TheWeb3Project <97693128+TheWeb3Project@users.noreply.github.com>
2022-06-05 00:12:42 +0900

    a

--- js/base.js
+++ js/base.js
@@ -686,6 +686,84 @@ function getTimeStr(t, d=true, h=true, m=true, s=true) {
   return timeStr;
 }
 
+///////////////////////////////// mouse
+function distanceBetween(point1, point2) {
+  return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
+}
+
+function angleBetween(point1, point2) {
+  return Math.atan2( point2.x - point1.x, point2.y - point1.y );
+}
+
+// Only test every `stride` pixel. `stride`x faster,
+// but might lead to inaccuracy
+function getFilledInPixels(stride) {
+  if (!stride || stride < 1) { stride = 1; }
+  
+  var pixels   = ctx.getImageData(0, 0, canvas.width, canvas.height),
+      pdata    = pixels.data,
+      l        = pdata.length,
+      total    = (l / stride),
+      count    = 0;
+  
+  // Iterate over all pixels
+  for(var i = count = 0; i < l; i += stride) {
+    if (parseInt(pdata[i]) === 0) {
+      count++;
+    }
+  }
+  
+  return Math.round((count / total) * 100);
+}
+
+function getMouse(e, canvas) {
+  var offsetX = 0, offsetY = 0, mx, my;
+
+  if (canvas.offsetParent !== undefined) {
+    do {
+      offsetX += canvas.offsetLeft;
+      offsetY += canvas.offsetTop;
+    } while ((canvas = canvas.offsetParent));
+  }
+
+  mx = (e.pageX || e.touches[0].clientX) - offsetX;
+  my = (e.pageY || e.touches[0].clientY) - offsetY;
+
+  return {x: mx, y: my};
+}
+
+let isDrawing = false;
+let lastPoint;
+function handleMouseDown(e) {
+  isDrawing = true;
+  lastPoint = getMouse(e, canvas);
+}
+
+function handleMouseMove(e) {
+  if (!isDrawing) { return; }
+  
+  e.preventDefault();
+
+  var currentPoint = getMouse(e, canvas),
+      dist = distanceBetween(lastPoint, currentPoint),
+      angle = angleBetween(lastPoint, currentPoint),
+      x, y;
+  
+  for (var i = 0; i < dist; i++) {
+    x = lastPoint.x + (Math.sin(angle) * i) - 25;
+    y = lastPoint.y + (Math.cos(angle) * i) - 25;
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.drawImage(brush, x, y);
+  }
+  
+  lastPoint = currentPoint;
+  handlePercentage(getFilledInPixels(32));
+}
+
+function handleMouseUp(e) {
+  isDrawing = false;
+}
+
 ///////////////////////////////// web3
 
 function BSC(type, txt) {
